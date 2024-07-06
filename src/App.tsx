85a5f0f73a5b16cf71a0a13484538fc1b1cdb7bf
andreeich <andreei.chervoniak@gmail.com>
2024-07-06 08:12:08 +0300

    tabs, tab, pin functionality, tabs localStorage sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,208 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+	ContextMenu,
+	ContextMenuContent,
+	ContextMenuItem,
+	ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+
+import Box from "./assets/box.svg";
+import Dashboard from "./assets/dashboard.svg";
+import Bank from "./assets/bank.svg";
+import PhoneCall from "./assets/phone-call.svg";
+import UserAdd from "./assets/user-add.svg";
+import Shop from "./assets/shop.svg";
+import ChartPie from "./assets/chart-pie.svg";
+import Mail from "./assets/mail.svg";
+import Settings from "./assets/settings.svg";
+import Book from "./assets/book.svg";
+import Cube from "./assets/cube.svg";
+import List from "./assets/list.svg";
+import ShoppingCart from "./assets/shopping-cart.svg";
+import Browser from "./assets/browser.svg";
+import Pin from "./assets/pin.svg";
+import { useEffect, useState } from "react";
+
+interface IntTab {
+	id: string;
+	name: string;
+	iconSrc: string;
+	isPinned: boolean;
+}
+
+const tabsData: IntTab[] = [
+	{
+		id: "1",
+		name: "Lagerverwaltung",
+		iconSrc: Box,
+		isPinned: true,
+	},
+	{
+		id: "2",
+		name: "Dashboard",
+		iconSrc: Dashboard,
+		isPinned: false,
+	},
+	{
+		id: "3",
+		name: "Banking",
+		iconSrc: Bank,
+		isPinned: false,
+	},
+	{
+		id: "4",
+		name: "Telefonie",
+		iconSrc: PhoneCall,
+		isPinned: false,
+	},
+	{
+		id: "5",
+		name: "Accounting",
+		iconSrc: UserAdd,
+		isPinned: false,
+	},
+	{
+		id: "6",
+		name: "Verkauf",
+		iconSrc: Shop,
+		isPinned: false,
+	},
+	{
+		id: "7",
+		name: "Statistik",
+		iconSrc: ChartPie,
+		isPinned: false,
+	},
+	{
+		id: "8",
+		name: "Post Office",
+		iconSrc: Mail,
+		isPinned: false,
+	},
+	{
+		id: "9",
+		name: "Administration",
+		iconSrc: Settings,
+		isPinned: false,
+	},
+	{
+		id: "10",
+		name: "Help",
+		iconSrc: Book,
+		isPinned: false,
+	},
+	{
+		id: "11",
+		name: "Warenbestand",
+		iconSrc: Cube,
+		isPinned: false,
+	},
+	{
+		id: "12",
+		name: "Auswahlilsten",
+		iconSrc: List,
+		isPinned: false,
+	},
+	{
+		id: "13",
+		name: "Einkauf",
+		iconSrc: ShoppingCart,
+		isPinned: false,
+	},
+	{
+		id: "14",
+		name: "Rechn",
+		iconSrc: Browser,
+		isPinned: false,
+	},
+	{
+		id: "15",
+		name: "Lagerverwaltung",
+		iconSrc: Box,
+		isPinned: false,
+	},
+	{
+		id: "16",
+		name: "Verkauf",
+		iconSrc: Shop,
+		isPinned: false,
+	},
+	{
+		id: "17",
+		name: "Post Office",
+		iconSrc: Mail,
+		isPinned: false,
+	},
+	{
+		id: "18",
+		name: "Telefonie",
+		iconSrc: PhoneCall,
+		isPinned: false,
+	},
+];
 
 function App() {
+	const [tabs, setTabs] = useState<IntTab[]>(() => {
+		const savedTabs = localStorage.getItem("tabs");
+		return savedTabs ? JSON.parse(savedTabs) : tabsData;
+	});
+	const [activeTabId, setActiveTabId] = useState<IntTab["id"]>(() => {
+		const savedTabId = localStorage.getItem("activeTabId");
+		return savedTabId ? savedTabId : tabs[0].id;
+	});
+
+	const handlePin = (tabItem: IntTab) => {
+		const newTab: IntTab = {
+			...tabItem,
+			isPinned: !tabItem.isPinned,
+		};
+		setTabs(tabs.map((t) => (t.id === tabItem.id ? newTab : t)));
+	};
+
+	const handleClick = (tabId: IntTab["id"]) => {
+		setActiveTabId(tabId);
+	};
+
+	useEffect(() => {
+		const tabsToSave = JSON.stringify(tabs, null, 2);
+		localStorage.setItem("tabs", tabsToSave);
+	}, [tabs]);
+	useEffect(() => {
+		localStorage.setItem("activeTabId", activeTabId);
+	}, [activeTabId]);
+
 	return (
-		<>
-			<Tabs defaultValue="account" className="w-[400px]">
-				<TabsList>
-					<TabsTrigger value="account">Account</TabsTrigger>
-					<TabsTrigger value="password">Password</TabsTrigger>
-				</TabsList>
-				<TabsContent value="account">
-					Make changes to your account here.
-				</TabsContent>
-				<TabsContent value="password">Change your password here.</TabsContent>
+		<div className="p-5">
+			<Tabs defaultValue={activeTabId} className="w-full">
+				<div className="max-w-full overflow-x-scroll">
+					<TabsList>
+						{tabs.map((t) => (
+							<ContextMenu key={t.id}>
+								<ContextMenuTrigger>
+									<TabsTrigger value={t.id} onClick={() => handleClick(t.id)}>
+										<div className="flex-shrink-0">
+											<img src={t.iconSrc} alt={t.name} />
+										</div>
+										{!t.isPinned ? t.name : undefined}
+									</TabsTrigger>
+								</ContextMenuTrigger>
+								<ContextMenuContent>
+									<ContextMenuItem onClick={() => handlePin(t)}>
+										<img src={Pin} alt="Pin" />
+										{t.isPinned ? "Unpin" : "Pin"} anpinnen
+									</ContextMenuItem>
+								</ContextMenuContent>
+							</ContextMenu>
+						))}
+					</TabsList>
+				</div>
+				{tabs.map((t) => (
+					<TabsContent key={t.id} value={t.id}>
+						{t.name}
+					</TabsContent>
+				))}
 			</Tabs>
-		</>
+		</div>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -17,6 +17,18 @@ module.exports = {
       },
     },
     extend: {
+      colors: {
+        background: {
+          default: "#FEFEFE",
+          active: "#F1F5F8",
+          drag: "#7F858D",
+          blue: "#4690E2",
+        },
+        foreground: {
+          default: "#7F858D",
+          active: "#343434",
+        },
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -34,4 +46,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
